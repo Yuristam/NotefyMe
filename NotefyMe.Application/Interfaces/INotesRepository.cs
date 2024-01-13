4d31f1dbe68ffc0f5f961f6adf7d395d0da8cbee
Yuristam <yuristamasanakunov@gmail.com>
2024-01-14 00:58:29 +0600

    create INotesRepository.js in Application and NoteRepository.js in Infrastructure

---
+++ NotefyMe.Application/Interfaces/INotesRepository.cs
@@ -0,0 +1,14 @@
+﻿using NotefyMe.Domain.Entities;
+
+namespace NotefyMe.Application.Interfaces
+{
+    public interface INotesRepository
+    {
+        Task<IEnumerable<Note>> GetAllNotesAsync();
+        Task<Note> GetNoteByIdAsync(int id);
+        bool Add(Note note);
+        bool Update(Note note);
+        bool Delete(Note note);
+        bool Save();
+    }
+}
---
+++ NotefyMe.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotefyMe.Application.Interfaces;
+using NotefyMe.Domain.Entities;
+using NotefyMe.Infrastructure.Data;
+
+namespace NotefyMe.Infrastructure.Repositories
+{
+    public class NoteRepository : INotesRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NoteRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Note note)
+        {
+            _context.Add(note);
+            return Save();
+        }
+
+        public bool Delete(Note note)
+        {
+            _context.Remove(note);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Note>> GetAllNotesAsync()
+        {
+            return await _context.Notes.ToListAsync();
+        }
+
+        public Task<Note> GetNoteByIdAsync(int id)
+        {
+            return _context.Notes.FirstOrDefaultAsync(n => n.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Note note)
+        {
+            _context.Update(note);
+            return Save();
+        }
+    }
+}
