9e3a557011cee2bcd79b724d5b7709bb5fde43ef
Yuristam <yuristamasanakunov@gmail.com>
2024-01-14 01:40:23 +0600

    add sql connection, implemented repository in Program.cs and create NoteController with Index page

---
+++ NotefyMe.WebApp/Controllers/NotesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotefyMe.Application.Interfaces;
+
+namespace NotefyMe.WebApp.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly INotesRepository _notesRepository;
+
+        public NotesController(INotesRepository notesRepository)
+        {
+            _notesRepository = notesRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var notes = await _notesRepository.GetAllNotesAsync();
+            return View(notes);
+        }
+    }
+}
