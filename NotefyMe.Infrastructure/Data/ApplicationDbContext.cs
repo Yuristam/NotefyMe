1646130796c5c9a1ef9b32d523dfa3b52ce96965
Yuristam <yuristamasanakunov@gmail.com>
2024-01-14 00:44:18 +0600

    add ApplicationDbContext.cs in Infrastructure and Project References

---
+++ NotefyMe.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotefyMe.Domain.Entities;
+
+namespace NotefyMe.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
