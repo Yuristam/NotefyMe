90e705131141d9e165d82865ae296c50a05ec82c
Yuristam <yuristamasanakunov@gmail.com>
2024-01-14 12:51:38 +0600

    add FirstMigration, install Microsoft.EntityFrameworkCore.Design NugetPackage in NotefyMe.WebApp and create Create.cshtml page for NotesController

---
+++ NotefyMe.Infrastructure/Migrations/20240114063052_InitFirstMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotefyMe.Infrastructure.Migrations
+{
+    public partial class InitFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NoteCategory = table.Column<int>(type: "int", nullable: false),
+                    NoteColor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
